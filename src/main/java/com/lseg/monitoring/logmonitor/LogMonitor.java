f2fbf682b1678906970f96a5e476be711de592bb
Serhan Tuygun <tuygun@gmail.com>
2025-05-27 22:17:51 +0100

    Add spring-boot as root and README.md with initial todos

---
+++ src/main/java/com/lseg/monitoring/logmonitor/LogMonitor.java
@@ -0,0 +1,11 @@
+package com.lseg.monitoring.logmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogMonitor {
+    public static void main(String[] args) {
+        SpringApplication.run(LogMonitor.class, args);
+    }
+}
